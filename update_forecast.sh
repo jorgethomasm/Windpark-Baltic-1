1007b03f73d9aa54dece1060051de0f39f071044
jorgethomasm <jorgethomasm@hotmail.com>
2024-08-07 14:49:29 +0000

    Creating project structure

---
+++ update_forecast.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+quarto render ./Baltic-1-energy-forecast.qmd --to html
+
+p=$(pwd)
+git config --global --add safe.directory $p
+
+if [[ "$(git status --porcelain)" != "" ]]; then
+    # quarto render ./docs/index.qmd
+    git config --global user.name $USER_NAME
+    git config --global user.email $USER_EMAIL    
+    git add data/*
+    git add docs/*
+    git commit -m "Forecast updated"
+    git push origin main
+else
+echo "Nothing to commit..."
+fi
