9c3361b310d64d89ef0c8a05a5c09b67b5f24db6
jorgethomasm <jorgethomasm@hotmail.com>
2024-09-06 12:03:10 +0000

    Developing a dataclass for wind turbine specs

--- src/utils/jthomwindlib.py
+++ src/utils/jthomwindlib.py
@@ -8,14 +8,61 @@ SPDX-FileCopyrightText: 2024 <jorgethomasm@ieee.org>
 SPDX-License-Identifier: MIT
 """
 
-import numpy as np  # transform Python to R (or MATLAB) hehehe
+import numpy as np  # transform Python to R or MATLAB
+import math
+from dataclasses import dataclass
 
-
+@dataclass
 class WindTurbine:
-    """ A custom and simple model of the requiered attributes of a Wind Turbine"""
+    """ 
+    A custom and simple model of the requiered attributes and
+    properties of a Wind Turbine
+    """
+    # ====== Wind Turbine Atributes (Specs) ======
 
-    pass
+    manufacturer: str  # e.g. Goldwin
+    model: str # e.g. GW 165-6.0 6300 
+    
+    # Location
+    latitude: float
+    longitude: float
 
+    rated_power: float # [kW]
+    rated_wind_speed: float # [m/s] at standard air density
+    area: float # squared metres
+    hub_height: float # metres
+    power_coefficient: float # Cp
+    rotor_diameter: float # metres
+
+    cut_in_speed: float # m/s
+    cut_out_speed: float # m/s
+
+    # Variable Speed - Variable pitch  
+
+    # Speed Range During Power Production
+    min_speed: float # [RPM]
+    max_speed: float # [RPM] Nominal
+
+    # Tip speed of blade 
+    @property
+    def min_tip_speed(self) -> float:
+        """
+        Linear speed of blade tip for Tip-Speed Ratio (lambda) calculation.
+        It returns Min. tip-speed in [m/s]
+        """
+        return 2*math.pi*(self.min_speed/60)*(self.rotor_diameter/2)
+    
+    @property
+    def max_tip_speed(self) -> float:
+        """
+        Linear speed of blade tip for Tip-Speed Ratio (lambda) calculation.
+        It returns Max. tip-speed in [m/s]
+        Sometimes this can be obtained directly from tech-spechs.
+        """
+        # max_tip_speed <- 92 # [m/s] # From Specs.
+
+        return 2*math.pi*(self.max_speed/60)*(self.rotor_diameter/2)
+   
 
 def calc_sat_water_vapour_press(temperature: np.ndarray) -> np.ndarray:
      
@@ -30,13 +77,13 @@ def calc_sat_water_vapour_press(temperature: np.ndarray) -> np.ndarray:
      c0 = 0.99999683
      c1 = -0.90826951e-2
      c2 = 0.78736169e-4
-     c3 =0.61117958e-6
-     c4 =.43884187e-8
-     c5 =0.29883885e-10
-     c6 =.21874425e-12
-     c7 =0.17892321e-14
-     c8 =.11112018e-16
-     c9 =0.30994571e-19
+     c3 = 0.61117958e-6
+     c4 = 0.43884187e-8
+     c5 = 0.29883885e-10
+     c6 = 0.21874425e-12
+     c7 = 0.17892321e-14
+     c8 = 0.11112018e-16
+     c9 = 0.30994571e-19
      
      pol =  c0+tem*(c1+tem*(c2+tem*(c3+tem*(c4+tem*(c5+tem*(c6+tem*(c7+tem*(c8+tem*(c9)))))))))
     
